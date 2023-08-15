e837542b9ad39034d6baebc2dccfdc32c8544f32
Igor Pereira <igor60.pereira@gmail.com>
2023-08-14 22:41:38 -0300

    test config routes

---
+++ App/Customer/Controller/CustomerController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Customer\Controller;
+class CustomerController
+{
+    public function saveCustomerAction()
+    {
+        die('adas');
+    }
+}
