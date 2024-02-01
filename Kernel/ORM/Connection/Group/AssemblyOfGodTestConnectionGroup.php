a3f326ed42af0ec38ce15e5e4efca8b6824c2a86
Igor Pereira <igor60.pereira@gmail.com>
2024-02-01 16:49:52 -0300

    CHORE: fix name user connection in database for test

--- Kernel/ORM/Connection/Group/AssemblyOfGodTestConnectionGroup.php
+++ Kernel/ORM/Connection/Group/AssemblyOfGodTestConnectionGroup.php
@@ -15,7 +15,7 @@ class AssemblyOfGodTestConnectionGroup extends Connection implements ConnectionG
     {
         $host       = $configuration->get('DB_TEST_HOST', 'mysql');
         $database   = $configuration->get('DB_TEST_DATABASE');
-        $user       = $configuration->get('DB_TEST_USERNAME');
+        $user       = $configuration->get('DB_TEST_USER');
         $password   = $configuration->get('DB_TEST_PASSWORD');
         $port       = $configuration->get('DB_TEST_PORT');
 
